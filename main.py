87e9a14a9052f322d8a9a57bc03737407ce1aa8f
DWBar <d.w.barbee@gmail.com>
2022-05-20 11:39:19 -0400

    Not a great start

---
+++ main.py
@@ -0,0 +1,156 @@
+print('''
+*******************************************************************************
+
+
+
+
+
+
+                          (
+
+
+ _.,-*~'^'~*-,._                        _.,-*~'^'~*-,._
+|               '*-,._            _.,-*'               '-,._
+|                     '*-,.__.,-*'                          '*-,.__.,-*,
+|                                                                      |
+`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'`'
+*******************************************************************************
+''')
+print("You find yourself in a seemingly endless desert.\nTo the NORTH, you see the sun beginning to peak above the horizon.")
+print("\nTo the EAST you see the faint outline of what may be buildings. \nIn the WEST you hear a rhythmic drumming.")
+print("\nYou cannot hear or see anything in the SOUTH but you feel something pulling you that direction.\n")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+levelOne = input("What Direction will you go? \n")
+
+NORTH = print('''
+                 \                        /\\\\\\\\\\\\\\\\\\\\
+    \                      /\\\\\\\\\\\\\\\\\\\\
+      \                    /\\\\\\\\\\\\\\\\\\
+       \\    \   /        /\\\\\\\\\\\\\\\\\
+        \\               \\\\\\\\\\\\\\\\\
+         \\   ()  - -   /\\\\\\\\\\\\\\\
+           \           /\\\\\\\\\\\\\
+            \\   \    /\\\\\\\\\\\\
+             \\\\    //\\\\\\\\\\\
+              \\\\\\///\\\\\\\\\
+               \\\\\\/\\\\\\\\\\\\
+    (\_         \\\\\\\\\\\\\\\\\\\
+   /   \
+   |    \
+    \    \
+     |  |
+     / /
+     ==
+   ====
+    ==
+
+              ''')
+print("You make your way NORTH and the sun begins to rapidly rise above the horizon.")
+print("As the sun stretches and surrounds your field of vision you realize that the body you woke up in is no longer under your command.")
+print("Faster and faster you move NORTH, the light and heat building as your body races to embrace the sun.")
+print("Your clothes burn and flake away and then your exposed skin begins darken into violent shades of red.")
+print("Your skin boils and sloughs away, releasing muscles and organs that spill out onto the desert floor.")
+print("Your body is striped to the bone but still runs further and further into the light.")
+print("Eventually the desert sand grinds down your bones and a trail of snow flake marrow drifts behind you.")
+print("As the last of this body settles into the dune you realize this was no desert of sand but a graveyard")
+print("of the countless others who decided to travel NORTH.")
+WAKE UP = print("Would you like to WAKE UP? \n")
+
+
+EAST = print('''
+                          _(
+             ___|_|_________
+            /___|_|_________\
+       ()  /_________________\
+   `'.()))/___________________\'-.'`'.
+  .,'(())()   ____     ____  |,.'     '-.
+     )(()))  |)~~(|   |)~~(| |. '-. ()`'.
+    ()()(()) ||__||   ||__|| | `'.,(())
+   ())()(()))________________|___ ()))()
+   ()((())()))| | | | | | | | | | (()()))
+  ()))(()()())|_|_|_|_|_|_|_|_|_|)(()(()
+  (()((())(()-------------------|(())(())
+  ~^~ ^" ^"  ^~^   ^"   ~^~    ^~^~(()(()
+  ^"     ^~^   ~^~   ^"    ^~^   ~~^~""^
+
+             ''')
+print("As you make your way EAST the sun begin to peak above the horizon to the NORTH.")
+print("What you originally thought were buildings turns out to be a simple house surrounded")
+print("by a small cosp of gnarled conifers. \n")
+print("In the front room of the house you find a large circular table at its center with")
+print("what looks like leather tightly stretched across its surface.")
+print("In the adjoining room you find a single bed.")
+print("The bed calls to you as a wave of drowsiness crashes over you.")
+print("As tired as you are you know that you need to make your way SOUTH as soon as possible.")
+input("What do you do? \n")
+
+SLEEP = ('''
+
+⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⣿⣇⣀⣀⣤⣤⣤⣴⣶⣶⣶⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⣿⣿⡿⠿⠟⠛⠛⠛⠉⣁⣤⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⣿⡇⠠⣶⣶⣾⣿⣷⠆⠙⠛⠛⣛⣉⠠⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⣿⣤⣀⡀⠉⠁⢠⡤⠶⠚⠋⣁⣠⣤⣴⣶⣶⣿⡿⠿⠗⠒⠀⣀⣀⣤⡄⠀
+⠀⠸⣿⣿⣿⣿⣿⣶⣤⣄⣀⠚⠻⠿⣿⡟⠛⢉⣉⣠⣤⣶⣶⣿⣿⣿⣿⣿⡇⠀
+⠀⢠⣤⡈⠙⠻⢿⣿⣿⣿⣿⣿⣶⣦⣤⡀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀
+⠀⠈⠉⠁⠀⠀⠀⠈⠉⠛⠿⢿⣿⣿⣿⡇⢸⣿⣿⣿⣿⣿⣿⠿⠟⠛⠋⣉⡁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⠇⠸⠿⠟⠛⠉⠁⠀⠀⠀⠀⢸⣿⠇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣦⣤⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⠿⠃⠀⠀⠀
+
+''')
+print("You lay down in the bed and immediately fall asleep.")
+print("A minute stretches to an hour, hours pass into a days, days to years.")
+print("As you're blissfully sleeping away years of your life the sun slowly creeps to its zenith.")
+print("At its peak the sun burns away the house leaving your body exposed to the bone white desert.")
+print("Your body bursts into a violent white pyre that lasts for the duration of the sun's journey back NORTH.")
+print("As soon as the sun slides below the horizon the pyre abruptly stops, only leaving a small pile of ash.")
+)
+
+
+SOUTH = print('''
+
+███▒▒░░░░─░░░▒▓█████████▓▓▒▒▒░░─────────
+████▓▓▒▒░░░────░▒▓▓██████████▓▒▒▒░░░────
+███████▓▓▓▒░░░─────░▒▒▓██████████▓▓▒░░░─
+████████████████▓▒▒░──░▒▒▒▓█████████▓▒░░
+█████████████████████▓▓▓▓▒░─░░▒▓▓▓▓▓▓▒▒░
+████████████████████████████▓▒░░░░░░░░──
+███████████████████████████████▒────────
+██████████████▓▓▒▒███▓███████████▒──────
+████████████▒░───░██▓▓█████▓███████░────
+████████████▒────░███▓█████▓██▓░▓███▒───
+█████████████░────▒██▓▓██▓███▓░──░███▓──
+██████████████─────▒████████▒─────▒████─
+███████████████▒─────▓████▓──────▒▓████▒
+█████████████████▓▒──────────▒▓████████▓
+█████████████████████▓▓▓▓▓▓████████████▒
+█████████▓▓▓▒▓▓▓███████████████████████▒
+███▓████▓▒░─────░░███████████▓▓▓▓▓▓▒░░░─
+██▓▓███▒░░─────────▓████████▒░░▒▒▒░░░───
+██▓███▓░░───────────███████▓░░░░░░░─────
+██▓███░────────░────▓██████▒────────────
+
+''')
+print("With every step SOUTH the light begins to fade until you're eventually left in an absolute darkness.")
+print("\nThe pull to the SOUTH is constant and building in strength.")
+print("\nYou realize the only way forward is to GIVE into the darkness")
+
+WEST = print('''
+
+                   .;--""```````""--;.
+                  /(                 )\
+                  \_`'-------------'`_/
+                  | `';-----;-----;'` |
+                  |   :     :     :   |
+                  | .' '. .' '. .' '. |
+                  \`.___.;.___.;.___.`/
+                   '...___________...'
+
+''')
+print("")
+print("")
+print("")
